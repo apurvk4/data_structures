e20192a31c64d2de879c17adde7d69ae67762dfb
apurv kumar <apurvkumar4299@gmail.com>
2020-08-12 12:31:36 +0530

    modified stack, now it uses queue internallly

--- stack/stack.h
+++ stack/stack.h
@@ -1,115 +1,29 @@
 #pragma once
+#include "../queue/queue.h"
+#include<initializer_list>
 template<typename T>
-class Node{
+class stack:public queue<T>{
     public:
-     T & get_data(){
-       return this->data_;
-     }
-    protected:
-      Node * head_=nullptr;
-      Node * tail_=nullptr;
-      void push_back(T &);
-      void pop_back();
-      Node(T &);
-      Node();
-     ~Node();
-      T data_;
-      Node *  next_=nullptr;
-      Node * prev_=nullptr;
+     stack()=default;
+     stack(std::initializer_list<T>);
+     ~stack()=default;
+     T & top();
+     void pop();
+    private:
+     T & front()=delete;
+     T & back()=delete;
 };
 template<typename T>
-void Node<T>::push_back(T & val){
-  if(this->head_==nullptr){
-    Node * temp=new Node(val);
-    this->head_=temp;
-    this->tail_=temp;
-  }else{
-    Node * temp=new Node(val);
-    this->tail_->next_=temp;
-    temp->prev_=this->tail_;
-    this->tail_=temp;
-  }
+T & stack<T>::top(){
+    return queue<T>::back();
 }
 template<typename T>
-void Node<T>::pop_back(){
-  if(head_ !=nullptr){
-    if(head_->next_ ==nullptr){
-      delete head_;
-      head_=nullptr;
-      tail_=nullptr;
-    }else{
-      Node * temp=tail_->prev_;
-      temp->next_=nullptr;
-      delete tail_;
-      tail_=temp;
-    } 
-  }
+void stack<T>::pop(){
+    this->pop_front();
+    this->size_-=1;
 }
 template<typename T>
-Node<T>::Node():prev_{nullptr},next_{nullptr},head_{nullptr},tail_{next_}{  
-}
-template<typename T>
-Node<T>::Node(T & val):data_{val},next_{nullptr},prev_{nullptr}{
-}
-template<typename T>
-Node<T>::~Node(){
-  if(head_ !=nullptr){
-    if(head_==tail_){
-        delete head_;
-        head_=nullptr;
-        tail_=nullptr;
-    }else{
-          Node *n1,*n2;
-          n1=head_;
-          while(n1 !=nullptr ){
-            n2=n1;
-            n1=n1->next_;
-            delete n2;
-          }
-          head_=nullptr;
-          tail_=nullptr;
-    }
-  }
-}
-template<typename T>
-class Stack:public Node<T>{
-  public:
-   Stack()=default;
-   ~Stack()=default;
-   void push(T &);
-   void pop();
-   T & top();
-   bool empty();
-   unsigned int size();
-  private:
-   T & get_data();
-   unsigned int count=0;
-};
-template<typename T>
-void Stack<T>::push(T & val){
-  this->push_back(val);
-  count++;
-}
-template<typename T>
-void Stack<T>::pop(){
-  this->pop_back();
-  count--;
-}
-template<typename T>
-T & Stack<T>::top(){
-  return this->tail_->get_data();
-}
-template<typename T>
-unsigned int Stack<T>::size(){
-  return count;
-}
-template<typename T>
-bool Stack<T>::empty(){
-  if(count>0){
-    return false;
-  }else{
-    return true;
-  }
+stack<T>::stack(std::initializer_list<T> l):queue<T>(l){
 }
 
 
